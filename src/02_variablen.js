1597cbee74e8321a8eb7968885c237de915a24c6
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-05-08 09:55:45 +0000

    Variablen vs. Konstanten in JS

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -27,24 +27,38 @@ console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // --> Hi, ich
     JS ist ein untypisierte Sprache!
 */
 
-let test;
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("-------------------");
+// let test;
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("-------------------");
 
-test = "hi"; // 'hi' string
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("-------------------");
+// test = "hi"; // 'hi' string
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("-------------------");
 
-test = 2; // number
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("-------------------");
+// test = 2; // number
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("-------------------");
 
-test = true; // boolean
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("-------------------");
+// test = true; // boolean
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("-------------------");
+
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+let test; // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe 
+
+// Konstante
+const test2 = "Hi!";  // Deklaration + Wertzuweisung gemeinsam !
+test2 = "hello"; // Laufzeitfehler !!
+console.log("Inhalt: " + test2); // Ausgabe 
+
 
 
