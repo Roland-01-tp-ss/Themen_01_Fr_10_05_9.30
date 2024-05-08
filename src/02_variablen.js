43efb51edbb8166d12b617d6b8eff1aca1b9c17b
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-05-08 07:32:38 +0000

    02_variablen.js ++

---
+++ src/02_variablen.js
@@ -0,0 +1,17 @@
+// Zeilenkommentar | Strg + #
+
+/**
+ * 
+ *  Block-Kommentar
+ * 
+ */
+
+/***** Variablen 01 *******/
+
+// console.log("Hi");
+// console.log(Hi);
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; 
+console.log(firstName);
