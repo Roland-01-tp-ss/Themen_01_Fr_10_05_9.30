7f7dfce3b62b3c5c11011c962413ff05dd7400b3
7Math <168723211+7Math@users.noreply.github.com>
2024-05-10 10:17:34 +0000

    Controlflow/if

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -2,7 +2,7 @@
 
 // Deklaration + Assignment
 const ageJohn = 30;
-const ageMark = 30;
+const ageMark = 25;
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
@@ -12,8 +12,21 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
-// console.log("isJohnOlder: " + isJohnOlder);
-// console.log("isJohnEqual: " + isJohnEqual);
-// console.log("------------------");
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+// if (false) // fake it!
+// if (isJohnOlder) // Verweis auf ein Testergebnis
+// if(ageJohn > ageMark) // Test
+
+{ // codeblock
+    console.log("John ist älter!");
+}
